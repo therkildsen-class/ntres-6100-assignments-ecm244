185da97cf9fd7b008b11d55873a3214f2cb9a410
ecm244 <ecm244@cornell.edu>
2025-10-02 10:31:41 -0400

    notes 10/2

--- course notes/importing_data.R
+++ course notes/importing_data.R
@@ -28,3 +28,19 @@ msa_clean <- clean_names(msa)
 parse_number("$100")
 parse_number("80%")
 parse_number("Costs $100")
+
+parse_double("1,23",locale=locale(decimal_mark=","))
+#in some countries a comma represents a decimal
+
+parse_number("123.456.1789", locale=locale(grouping_mark = "."))
+#and sometimes a decimal point is used to seperate thousands. 
+
+mess = read_tsv("https://raw.githubusercontent.com/nt246/
+NTRES-6100-data-science/refs/heads/main/datasets/messy_data.tsv", 
+locale = locale(decimal_mark = ","))
+
+mess = read_tsv("https://raw.githubusercontent.com/nt246/
+          NTRES-6100-data-science/refs/heads/main/datasets/messy_data.tsv",
+            locale = locale(decimal_mark = ","), na = c("Missing", "N/A")) 
+
+#changes "missing to NA". Use the "problems()" function to show problems
