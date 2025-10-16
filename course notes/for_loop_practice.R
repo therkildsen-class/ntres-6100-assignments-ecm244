0edfab511e5dce212a5386998496ebb52616f00a
ecm244 <ecm244@cornell.edu>
2025-10-16 11:03:44 -0400

    10/16

---
+++ course notes/for_loop_practice.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(gapminder)
+
+gapminder <- gapminder |> 
+  rename("life_exp" = lifeExp, "gdp_per_cap" = gdpPercap)
+
+dir.create("gap_figs") 
+
+## create a list of countries
+country_list <- c("Spain","Sweden","Mexico")
+
+for (cntry in country_list) {
+  
+  ## filter the country to plot
+  gap_to_plot <- gapminder |>
+    filter(country == cntry)
+  
+  ## plot
+  my_plot <- ggplot(data = gap_to_plot, aes(x = year, y = gdp_per_cap)) + 
+    geom_point() +
+    ## add title and save
+    labs(title = str_c(cntry, "GDP per capita", sep = " "))
+  
+  ## add the gap_figs/ folder
+  ggsave(filename = str_c("gap_figs/", cntry, "_gdp_per_cap.png", sep = ""), plot = my_plot)
+} 
