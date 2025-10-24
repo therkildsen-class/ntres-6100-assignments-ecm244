c3fdc26618a335b33322f86b669b54250e1bc0e4
ecm244 <ecm244@cornell.edu>
2025-10-24 14:38:11 -0400

    start lab 9

---
+++ labs/lab9.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(stringr) #install.packages("stringr")
+
+# create a function that tells DNA from RNA based on if it has a U
+
+dna_or_rna <- function(sequence) {
+  if (str_detect(sequence, "U")) {
+    return("RNA")
+  } else {
+    return("DNA")
+  }
+}
+  
+#test
+
+dna_or_rna("attggc")
+
+dna_or_rna("gccaau")
+
+dna_or_rna("ccagac")
+
+dna_or_rna("tgcacug")
+
+
+#test with a for loop 
+
+sequences = c("ttgaatgccttacaactgatcattacacaggcggcatgaagcaaaaatatactgtgaaccaatgcaggcg", 
+              "gauuauuccccacaaagggagugggauuaggagcugcaucauuuacaagagcagaauguuucaaaugcau", 
+              "gaaagcaagaaaaggcaggcgaggaagggaagaagggggggaaacc", 
+              "guuuccuacaguauuugaugagaaugagaguuuacuccuggaagauaauauuagaauguuuacaacugcaccugaucagguggauaaggaagaugaagacu", 
+              "gataaggaagaugaagacutucaggaaucuaauaaaaugcacuccaugaauggauucauguaugggaaucagccggguc")
+
+for (seq in sequences) {
+  dna_or_rna(seq)
+}
